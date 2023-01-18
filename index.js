0a1f3f5ed9a99499c1fa2422bc7fafa0e7cf69d7
oscar <74271738+oscarpat@users.noreply.github.com>
2023-01-18 02:04:36 +0000

    ejercicios

---
+++ index.js
@@ -0,0 +1,14 @@
+/*Usando console.log()*/
+console.log("Hello World!****")
+
+/*Variables de registro*/
+var foo = "bar";
+console.log(foo);
+
+var thisVar = 'first value';
+var thatVar = 'seconsd value';
+console.log("thisVar:", thisVar, "and thatVar:", thatVar );
+
+/*You can use console.log() in combination with placeholders:*/
+var greet = "Hello", who = "World";
+console.log("%s, %s!", greet, who);
